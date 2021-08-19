c9b49e7929921408283f23d91bfef4e8f8f82acf
ImMega <72713009+ImMega@users.noreply.github.com>
2021-08-19 10:39:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -225,6 +225,8 @@ client.on("message", message => {
     if (jebanje) {
          message.reply("i ja tebi isto!");
     }
+    
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
