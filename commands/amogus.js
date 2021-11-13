411311aa2ae6c6939ebe32ffbc789156b77e90ef
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-13 23:46:47 +0100

    shitpost

--- commands/amogus.js
+++ commands/amogus.js
@@ -4,6 +4,6 @@ module.exports = {
     description: "amogus",
     usage: "amogus",
     execute(message){
-        message.channel.send({ files: ["https://c.tenor.com/2sVF1kmIy2wAAAAC/amogus.gif"] });
+        message.channel.send("https://tenor.com/view/amogus-gif-21498845");
     }
 }
