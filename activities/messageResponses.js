db2c54423e4dbda2022925eea320f0d0a496df37
ImMega <72713009+ImMega@users.noreply.github.com>
2021-12-16 00:22:08 +0100

    Update messageResponses.js

--- activities/messageResponses.js
+++ activities/messageResponses.js
@@ -1,3 +1,7 @@
 module.exports = (message) => {
-    if(message.content.toLowerCase() === "jebem ti mater") message.reply("I ja tebi isto!");
+    if(message.content.toLowerCase() === "jebem ti mater") {
+        message.channel.sendTyping();
+        
+        setTimeout(() => { message.reply("I ja tebi isto!"); }, 3000)
+    }
 }
